077ba048b1b173ee4aecd99291d2fd9ddd217c22
陈彦宋 <3182936996@qq.com>
2023-11-05 22:44:32 +0800

    pinia独立维护

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import './assets/main.scss'
+import pinia from './stores'
 
 const app = createApp(App)
 
-app.use(createPinia().use(piniaPluginPersistedstate))
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
