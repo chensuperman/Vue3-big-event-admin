915f661ff9d5241199d4a8f0085da0fdc74ea30a
陈彦宋 <3182936996@qq.com>
2023-11-05 21:23:37 +0800

    router使用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+//在Vue3 CompositionAPI中
+//1.获取路由对象 router usePouter
+//  const router=useRouter()
+//2.获取路由参数 route useRouter
+//  const router =useRoute()
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const goList = () => {
+  router.push('/list')
+  console.log(route)
+}
+</script>
 
 <template>
   <div>我是App</div>
+  <button @click="$router.push('/home')">跳首页</button>
+  <button @click="goList">跳列表页</button>
 </template>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+//  createRouter 创建路由实例
+//  配置history的模式
+//  1.history模式：createWebHistory   地址栏不带"#"
+//  2.hash模式：createHashHistory     地址栏带"#"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
