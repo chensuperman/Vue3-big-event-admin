d7d916098d624e116df509bcaa528ee3cec53819
陈彦宋 <3182936996@qq.com>
2023-11-05 21:39:45 +0800

    Element Plus使用

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,11 @@ const goList = () => {
 
 <template>
   <div>我是App</div>
-  <button @click="$router.push('/home')">跳首页</button>
-  <button @click="goList">跳列表页</button>
+  <el-button @click="$router.push('/home')">跳首页</el-button>
+  <el-button @click="goList">跳列表页</el-button>
+
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
 </template>
 
 <style scoped></style>
